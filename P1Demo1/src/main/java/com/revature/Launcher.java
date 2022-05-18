807a9707e4c90b085d4d1f3fcfb57a82d3a28fd8
Your Name <you@example.com>
2022-05-18 18:22:02 -0400

    Javalin update

--- P1Demo1/src/main/java/com/revature/Launcher.java
+++ P1Demo1/src/main/java/com/revature/Launcher.java
@@ -1,21 +1,42 @@
 package com.revature;
 import java.util.Scanner;
 
+import Controller.AuthController;
+import Controller.ReimbursementController;
+import Controller.UserController;
 import Models.Roles;
 import Models.Users;
 import Service.CLI_Menu_Service;
 import Service.User_Services;
+import io.javalin.Javalin;
 
 public class Launcher {
 	public static void main(String[] args) {
 		
+		AuthController ac = new AuthController();
+		UserController uc = new UserController();
+		ReimbursementController rc = new ReimbursementController();
+		
+		
 		CLI_Menu_Service options = new CLI_Menu_Service();
 		options.displayMenu();
-		//User_Services e = new User_Services();
-		//e.idExists(1);
 		
+	Javalin app = Javalin.create(
+				config -> {
+					config.enableCorsForAllOrigins(); //This is what allows teh server to process JS requests from anywhere
+				}
+			).start(3000);
 		
+			//Now we need our endpoints
+			//app.get("/employee", ec.getEmployeesHandler);
+			
+			//app.post("/employee", ec.insertEmployeesHandler);
 		
 	}
-
+	
+	/*public void start(int port) {
+		this.app.start(port);
+	}*/
 }
+
+
